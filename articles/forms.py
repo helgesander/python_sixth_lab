460dc1c7b9c749f2c210a0f13fde61e4e9b2aba9
helgesander <helgesander1006@list.ru>
2022-10-28 08:08:44 +0300

    Created UserRegistrationForm

--- articles/forms.py
+++ articles/forms.py
@@ -1,9 +1,35 @@
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms.forms import Form
+from django import forms
+from django.contrib import messages
 
-class UserRegistrationForm(forms.Form):
+
+class UserRegistrationForm(forms.ModelForm):
     username = forms.CharField(label="Логин", max_length=100)
     email = forms.EmailField(label="Email", max_length=100)
-    password = forms.PasswordInput()
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+
+    def check(self):
+        try:
+            User.objects.get(username=self.username)
+        except User.DoesNotExist:
+            print("Пользователь с таким логином уже существует")
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password']
+        )
+        return user
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label="Логин", max_length=100, error_messages={'empty' : 'Введите логин! '})
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput, error_messages={'empty': 'Введите пароль! '})
+
+
 
--- articles/views.py
+++ articles/views.py
@@ -4,8 +4,6 @@ from .models import Article
 from django.http import Http404
 from django.shortcuts import redirect
 from .forms import UserRegistrationForm
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
 
 
 def get_article(request, article_id):
@@ -14,8 +12,6 @@ def get_article(request, article_id):
         return render(request, 'article.html', {"post": post})
     except Article.DoesNotExist:
         raise Http404
-
-
 
 def archive(request):
     return render(request, 'archive.html', {"posts": Article.objects.all()})
@@ -49,51 +45,24 @@ def create_post(request):
     else:
         raise Http404
 
-# Create your views here.
 
 def error_registration(request):
     return render(request, 'error_registration.html')
-'''
-def registration(request):
-    if not request.user.is_anonymous:
-        #return render(request, 'registration.html')
-        if request.method == "POST":
-            form = {
-                'login': request.POST['login'],
-                'email': request.POST['email'],
-                'password': request.POST['password']
-            }
-            if form['login'] and form['email'] and form['password']:
-                try:
-                    User.objects.get(username=form['login'])
-                    return redirect('error_registration')
-                except User.DoesNotExist:
-                    User.objects.create_user(form['login'], form['email'], form['password'])
-                    return render(request, 'registration.html', {'form': form})
 
-        else:
-            raise Http404
-
-def login(request):
-    return render(request, 'login.html', {})
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration.html'
-    
-'''
 
 def registration(request):
     if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid:
-            if User.objects.get(username=form.username):
-                True
-                #djsjdsd
-            else:
-                User.objects.create_user(form.username, form.email, form.password)
-                return render(request, 'registration.html', {'form': form})
+        new_user = UserRegistrationForm(request.POST)
+        if new_user.is_valid():
+            new_user.save()
+
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration.html', context)
+
 
 
 
